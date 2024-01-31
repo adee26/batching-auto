44ddd4ba48ff37210ac235fd3d0d5bf4e8bb437e
Adelina Apetrei <adelina.apetrei26@gmail.com>
2024-01-31 16:20:43 +0200

    Add job parameters

--- src/main/java/com/adedev/batchingauto/config/jpa/JpaItemReader.java
+++ src/main/java/com/adedev/batchingauto/config/jpa/JpaItemReader.java
@@ -2,10 +2,13 @@ package com.adedev.batchingauto.config.jpa;
 
 import com.adedev.batchingauto.entity.postgres.StudentPostgres;
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 public class JpaItemReader {
@@ -16,11 +19,17 @@ public class JpaItemReader {
     }
 
     @Bean
-    public JpaCursorItemReader<StudentPostgres> jpaPostgresCursorItemReader() {
+    @StepScope
+    public JpaCursorItemReader<StudentPostgres> jpaPostgresCursorItemReader(
+            @Value("#{jobParameters['currentItemCount']}") Integer currentItemCount,
+            @Value("#{jobParameters['maxItemCount']}") Integer maxItemCount) {
         JpaCursorItemReader<StudentPostgres> jpaCursorItemReader = new JpaCursorItemReader<>();
 
         jpaCursorItemReader.setEntityManagerFactory(postgreSqlEntityManagerFactory);
         jpaCursorItemReader.setQueryString("FROM StudentPostgres");
+
+        jpaCursorItemReader.setCurrentItemCount(currentItemCount);
+        jpaCursorItemReader.setMaxItemCount(maxItemCount);
 
         return jpaCursorItemReader;
     }
